fa87f038eb8ce2ef9bfc1b858b6b8639c215d9a1
donjor <donjordev@gmail.com>
2018-05-31 23:20:05 +1200

    init commit with haven address prefixes

--- monero_utils/monero_config.js
+++ monero_utils/monero_config.js
@@ -1,21 +1,21 @@
 // Copyright (c) 2014-2018, MyMonero.com
-// 
+//
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //	conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //	of conditions and the following disclaimer in the documentation and/or other
 //	materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its contributors may be
 //	used to endorse or promote products derived from this software without specific
 //	prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -30,7 +30,7 @@
 //
 const JSBigInt = require('../cryptonote_utils/biginteger').BigInteger
 //
-module.exports = 
+module.exports =
 {
 	// Number of atomic units in one unit of currency. e.g. 12 => 10^12 = 1000000000000
 	coinUnitPlaces: 12,
@@ -39,28 +39,26 @@ module.exports =
 	txMinConfirms: 10,
 
 	// Currency symbol
-	coinSymbol: 'XMR',
+	coinSymbol: 'XHV',
 
 	// OpenAlias prefix
-	openAliasPrefix: "xmr",
+	openAliasPrefix: "xhv",
 
 	// Currency name
-	coinName: 'Monero',
+	coinName: 'Haven',
 
 	// Payment URI Prefix
-	coinUriPrefix: 'monero:',
+	coinUriPrefix: 'haven:',
 
 	// Prefix code for addresses
-	addressPrefix: 18, // 18 => addresses start with "4"
-	integratedAddressPrefix: 19,
-
-	// Network per kb fee in atomic units
-	feePerKB_JSBigInt: new JSBigInt('2000000000'), // 0.002
+	addressPrefix: 23284, // 18 => addresses start with "4"
+	integratedAddressPrefix: 841588,
+	subaddressPrefix: 1235316,
 
 	// Dust threshold in atomic units
-	// 10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-	dustThreshold: new JSBigInt('10000000000'),
-	
+	// 2*10^9 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	dustThreshold: new JSBigInt('2000000000'),
+
 	// Maximum block number, used for tx unlock time
 	maxBlockNumber: 500000000,
 
